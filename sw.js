23f7e40c4d3a555d68a1ca69692b82b616bc6d48
RITHIK Ahlawat <76678502+rithik0007@users.noreply.github.com>
2022-06-10 15:49:22 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// on installaion
+
+
+self.addEventListener('install' , function(event){
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache){
+      return cache.addAll (
+      [  '/calculator.html',
+        '/css/calculator1.css'
+      ]
+      );
+    });
+  );
+});
+
+
+// with request network
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
